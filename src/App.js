35f052e351ccf463909eb375fd44320a5f8bdbf9
Osama Saeed <osdroidi@gmail.com>
2023-02-24 03:50:22 +0300

    Add react-github-corner and GithubCorner component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/Header";
 import Section from "./components/Section";
 import { DataProvider } from "./components/Context";
+import GithubCorner from 'react-github-corner';
 
 class App extends React.Component {
   render() {
@@ -12,6 +13,7 @@ class App extends React.Component {
           <Router>
             <Header />
             <Section />
+            <GithubCorner href="https://github.com/OsDroidi/simple-e-commerce-react-js" target="_blank" />
           </Router>
         </div>
       </DataProvider>
